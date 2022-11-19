012e6010861410957876f8a4587f6662ce4b23be
radsi <78619513+radsi@users.noreply.github.com>
2022-11-19 17:48:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+const express = require("express");
+const app = express();
+
+let discountSteamGames = {};
+let freeEpicGames = {};
+
+async function getDiscountGames(){
+    var gamesInfo = await axios.get("http://store.steampowered.com/api/featuredcategories/?l=spanish");
+    gamesInfo.data.specials.items.forEach(game => {
+        if(game.final_price < game.original_price){
+            discountSteamGames[game.name] = {image: game.large_capsule_image, price: game.final_price, game_url: `https://store.steampowered.com/app/${game.id}`};
+        }
+    });
+
+    gamesInfo = await axios.get("https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions");
+    gamesInfo.data.data.Catalog.searchStore.elements.forEach(game =>{
+        if(game.price.totalPrice.discountPrice == 0){
+            var gameNameURL;
+            game.customAttributes.forEach(x => {
+                if(x.key == "com.epicgames.app.productSlug"){
+                    gameNameURL = x.value;
+                }
+            });
+            freeEpicGames[game.title] = {image: game.keyImages[2].url, game_url: `https://store.epicgames.com/es-ES/p/${gameNameURL}`}
+        }
+    });
+}
+
+app.listen(4488, ()=>{
+    getDiscountGames();
+    console.log("Server listening on port 3000");
+});
+
+app.get("/getSteam", (req, res)=>{
+    getDiscountGames();
+    res.json(discountSteamGames);
+});
+
+app.get("/getEpic", (req, res)=>{
+    getDiscountGames();
+    res.json(freeEpicGames);
+});
