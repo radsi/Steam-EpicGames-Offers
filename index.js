7f8618e439acb3469df932a93652b23f6e90ae7b
radsi <78619513+radsi@users.noreply.github.com>
2022-11-19 18:36:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,47 +5,38 @@ const app = express();
 let discountSteamGames = {};
 let freeEpicGames = {};
 
-async function getDiscountGames(){
+async function getSteamDiscountGames(){
     var gamesInfo = await axios.get("http://store.steampowered.com/api/featuredcategories/?l=spanish");
-    var isFree;
     gamesInfo.data.specials.items.forEach(game => {
         if(game.final_price < game.original_price){
             discountSteamGames[game.name] = {image: game.large_capsule_image, price: game.final_price, game_url: `https://store.steampowered.com/app/${game.id}`};
         }
     });
+}
 
-    gamesInfo = await axios.get("https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions");
-
-
-
-    gamesInfo.data.data.Catalog.searchStore.elements.forEach(game =>{
-        game.categories.forEach(x => {
-            if(x.path == "freegames"){
-                var gameNameURL;
-                game.customAttributes.forEach(catt => {
-                    if(catt.key == "com.epicgames.app.productSlug"){
-                        gameNameURL = x.value;
-                    }
-                });
-                freeEpicGames[game.title] = {image: game.keyImages[2].url, game_url: `https://store.epicgames.com/es-ES/p/${gameNameURL}`}
-            }
-        });
+async function getFreeEpicGames(){
+    var gamesInfo = await require("free-games")("es", "ES");
+    gamesInfo.forEach(game => {
+        if(game.price.totalPrice.discountPrice == 0){
+            game.keyImages.forEach(x => {
+                if(x.type == "Thumbnail"){
+                    freeEpicGames[game.title] = {image: x.url, game_url: `https://store.epicgames.com/es-ES/p/${game.productSlug}`}
+                }
+            });
+        }      
     });
-
-    
 }
 
 app.listen(4488, ()=>{
-    getDiscountGames();
     console.log("Server listening on port 3000");
 });
 
-app.get("/getSteam", (req, res)=>{
-    getDiscountGames();
+app.get("/getSteam", async (req, res)=>{
+    await getSteamDiscountGames();
     res.json(discountSteamGames);
 });
 
-app.get("/getEpic", (req, res)=>{
-    getDiscountGames();
+app.get("/getEpic", async (req, res)=>{
+    await getFreeEpicGames();
     res.json(freeEpicGames);
 });
