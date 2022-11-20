c444c327d2ed52b3876b15e954c4cf1adf9afe39
radsi <78619513+radsi@users.noreply.github.com>
2022-11-20 17:49:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ const app = express();
 let discountSteamGames = {};
 let freeEpicGames = {};
 
+function st(str){
+    return str.replace(/./g, function(chr){
+      return chr + '\u0336';
+    });
+  }
+
 async function getSteamDiscountGames(){
+    discountSteamGames = {};
     var gamesInfo = await axios.get("http://store.steampowered.com/api/featuredcategories/?l=spanish");
     gamesInfo.data.specials.items.forEach(game => {
         if(game.final_price < game.original_price){
-            discountSteamGames[game.name] = {image: game.large_capsule_image, price: game.final_price, game_url: `https://store.steampowered.com/app/${game.id}`};
+            discountSteamGames[game.name] = {image: game.large_capsule_image, price: game.final_price, discount: game.discount_percent, original_price: st(String(game.original_price/100)), game_url: `https://store.steampowered.com/app/${game.id}`};
         }
     });
 }
 
 async function getFreeEpicGames(){
+    freeEpicGames = {};
     var gamesInfo = await require("free-games")("es", "ES");
     gamesInfo.forEach(game => {
         if(game.price.totalPrice.discountPrice == 0){
@@ -28,7 +36,7 @@ async function getFreeEpicGames(){
 }
 
 app.listen(4488, ()=>{
-    console.log("Server listening on port 3000");
+    console.log("Server listening on port 4488");
 });
 
 app.get("/getSteam", async (req, res)=>{
