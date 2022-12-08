f94fd0da143a5de6bf0494a86f6e4f4bf1d84628
radsi <78619513+radsi@users.noreply.github.com>
2022-12-08 14:46:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -9,27 +9,35 @@ function st(str){
     return str.replace(/./g, function(chr){
       return chr + '\u0336';
     });
-  }
+}
 
 async function getSteamDiscountGames(){
-    var gamesInfo = await axios.get("http://store.steampowered.com/api/featuredcategories?l=spanish", {headers: {'Content-Type': 'application/json','Cache-Control' : 'no-cache'}});
-    gamesInfo.data.specials.items.forEach(game => {
-        console.log(game)
-        discountSteamGames[game.name] = {type: game.type, image: game.large_capsule_image, price: game.final_price, discount: game.discount_percent, original_price: st(String(game.original_price/100)), game_url: (game.type ? `https://store.steampowered.com/sub/${game.id}` : `https://store.steampowered.com/app/${game.id}`)};
-    });
+    var gamesInfo = await axios.get("http://store.steampowered.com/api/featuredcategories", {headers: {'Content-Type': 'application/json','Cache-Control' : 'no-cache'}});
+    gamesInfo = gamesInfo.data.specials.items;
+    for(let i = 0; i < gamesInfo.length; i++){
+        let amount = parseFloat(gamesInfo[i].final_price);
+        let from = gamesInfo[i].currency;
+        let to = "EUR";
+
+        let exchangeData = await axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}/${to}.json`);
+        let toValue = exchangeData.data[to];
+        let result = amount * toValue;
+        discountSteamGames[gamesInfo[i].name] = {image: gamesInfo[i].large_capsule_image, price: result, discount: gamesInfo[i].discount_percent, original_price: st(String(result/100)), game_url: (gamesInfo[i].type ? `https://store.steampowered.com/sub/${gamesInfo[i].id}` : `https://store.steampowered.com/app/${gamesInfo[i].id}`)};
+    }
 }
 
 async function getFreeEpicGames(){
     var gamesInfo = await axios.get("https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions", {headers: {'Content-Type': 'application/json','Cache-Control' : 'no-cache'}});
-    gamesInfo.data.data.Catalog.searchStore.elements.forEach(game =>{
-        if(game.promotions != null){
-            game.keyImages.forEach(x => {
+    gamesInfo = gamesInfo.data.data.Catalog.searchStore.elements;
+    for(let i = 0; i < gamesInfo.length; i++){
+        if(gamesInfo[i].promotions != null){
+            gamesInfo[i].keyImages.forEach(x => {
                 if(x.type == "Thumbnail"){
-                    freeEpicGames[game.title] = {image: x.url, game_url: `https://store.epicgames.com/es-ES/p/${(game.productSlug == null ? game.urlSlug : game.productSlug)}`}
+                    freeEpicGames[gamesInfo[i].title] = {image: x.url, game_url: `https://store.epicgames.com/es-ES/p/${(gamesInfo[i].productSlug == null ? gamesInfo[i].urlSlug : gamesInfo[i].productSlug)}`}
                 }
             });
         }
-    });
+    }
 }
 
 app.listen(4488, ()=>{
